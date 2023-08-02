ed8af7b977e5759d0dd6a0840865ae770b4c32b4
Vicente Garcia Andrade <vicgarci@c1r8s5.42madrid.com>
2023-08-02 12:51:10 +0200

    Añaddido shell lvl, revisados los fallos

--- srcs/startup/clone_env.c
+++ srcs/startup/clone_env.c
@@ -6,11 +6,13 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 10:19:52 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/07/12 18:04:41 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/08/02 11:30:12 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+t_bool	update_sshell_lvl(t_shell *shell);
 
 //Libera el env en caso de error
 static t_bool	free_clone_env(char **env)
@@ -55,5 +57,35 @@ t_bool	clone_env(char **env, t_shell *shell)
 	}
 	local_env[i] = NULL;
 	shell->env = local_env;
+	if (!update_sshell_lvl(shell))
+		return (false);
 	return (true);
+}
+
+t_bool	update_sshell_lvl(t_shell *shell)
+{
+	char	*s;
+	char	*s_value;
+	int		pos_shell_lvl;
+	int		value;
+
+	pos_shell_lvl = find_string(shell->env, "SHLVL");
+	s = shell->env[pos_shell_lvl];
+	s += 6;
+	value = ft_atoi(s);
+	value++;
+	s_value = ft_itoa(value);
+	if (s_value)
+	{
+		s = ft_strjoin("SHLVL=", s_value);
+		if (s)
+		{
+			free(s_value);
+			free(shell->env[pos_shell_lvl]);
+			shell->env[pos_shell_lvl] = s;
+			return (true);
+		}
+		return (false);
+	}
+	return (false);
 }
